6c2fbb68e394b22f08bb97815b194a425bedbf62
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2023-03-31 10:32:23 +0900

    [정길연] 백준 1157번 문제풀이

---
+++ week01/day2/boj1157/정길연_1157.java
@@ -0,0 +1,38 @@
+package day2.boj1157;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 정길연_1157 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        str = str.toUpperCase(Locale.ROOT);
+
+        int[] alph = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            alph[str.charAt(i) - 'A'] ++;
+        }
+
+        int max = -1;
+        int idx = -1;
+        for (int i = 0; i < alph.length; i++) {
+            if (max < alph[i]) {
+                max = alph[i];
+                idx = i;
+            }
+        }
+
+        Arrays.sort(alph);
+
+        if(alph[alph.length-1] == alph[alph.length-2]) System.out.println("?");
+        else {
+            char result = (char) (idx + 'A');
+            System.out.println(result);
+        }
+
+        br.close();
+    }
+}
